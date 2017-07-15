cbfa57dac0213868eb2e9f30960910d56f4b6226
J Miles <j.milesy.nz@gmail.com>
2017-07-15 14:33:51 +1200

    Fix tapering of tall pieces

--- lib/printed/round_piece.rb
+++ lib/printed/round_piece.rb
@@ -2,24 +2,24 @@ class RoundPiece < SolidRuby::Printed
   RoundPiece::GOLDEN = 1.618
   def initialize(tall=false, divet=false)
     @d = 25
-    @h = @d #@d * RoundPiece::GOLDEN
+    @h = @d
     @tall = tall
     @divet = divet
   end
 
   def part(_show)
-    res = cylinder(h: @h, d: @d)
     inset = @d / 10.0
+    slope_h = 2
+    res = cylinder(h: @h - slope_h, d: @d)
 
     if @tall
-      gap_h = (@d / RoundPiece::GOLDEN) / 2.0
-      res = res.translate(z: @h + gap_h)
-      res += cylinder(h: @h, d: @d)
-      res += cylinder(h: inset, d1: @d, d2: @d-inset).translate(z: @h)
-      res += cylinder(h: inset, d1: @d-inset, d2: @d).translate(z: @h + gap_h-inset)
-      #res -= cylinder(h: gap_h + 2, d: @d-inset).translate(z: @h - 1)
-      res += cylinder(h: gap_h, d: @d-inset).translate(z: @h)
+      gap_h = @h/10.0
+      res = res.translate(z: @h + gap_h + slope_h)
+      res += cylinder(h: @h - slope_h, d: @d)
+      res += cylinder(h: slope_h, d1: @d, d2: @d-inset).translate(z: @h - slope_h)
+      res += cylinder(h: slope_h, d1: @d-inset, d2: @d).translate(z: @h + gap_h)
 
+      res += cylinder(h: gap_h, d: @d-inset).translate(z: @h)
     end
 
     if @divet
--- lib/printed/square_piece.rb
+++ lib/printed/square_piece.rb
@@ -10,31 +10,40 @@ class SquarePiece < SolidRuby::Printed
   def part(_show)
     fill_r = @w / 5.0
     inset = @w / 10.0
-    gap_h = (@w/ RoundPiece::GOLDEN)/2.0
+    gap_h = @h/10.0#(@w/ RoundPiece::GOLDEN)/2.0
+    slope_h = 2
 
-    res = cube(@w, @w, @h).center_xy.fillet(fill_r)
+    res = cube(@w, @w, @h-slope_h).center_xy.fillet(fill_r)
 
     if @tall
-      res = res.translate(z: @h + gap_h)
-      res += cube(@w, @w, @h).center_xy.fillet(fill_r)
+      res = res.translate(z: @h + gap_h + slope_h)
+      res += cube(@w, @w, @h - slope_h).center_xy.fillet(fill_r)
+
+      res += hull(
+        cube(@w, @w, 0.1)
+          .center_xy
+          .fillet(fill_r),
+        cube(@w-inset, @w-inset, 0.1)
+          .center_xy
+          .fillet(fill_r)
+          .translate(z: slope_h)
+        )
+          .translate(z: @h - slope_h)
+
+        res += hull(
+          cube(@w, @w, 0.1)
+            .center_xy
+            .fillet(fill_r),
+          cube(@w-inset, @w-inset, 0.1)
+            .center_xy
+            .fillet(fill_r)
+            .translate(z: -slope_h)
+          )
+            .translate(z: @h + gap_h + slope_h)
 
       res += cube(@w-inset, @w-inset, gap_h)
       .center_xy.fillet(fill_r).translate(z: @h)
-      # gap += cylinder(d1: @w*1.4, d2: @w*1.4-inset, h: 1, fn: 4)
-      #   .rotate(z: 45)
-      #   .translate(x: 0, z: @h)
-      #
-      # gap += cylinder(d1: @w*1.4-inset, d2: @w*1.4, h: 1, fn: 4)
-      #   .rotate(z: 45)
-      #   .translate(x: 0, z: @h+gap_h-1)
-      #
-      # res += (gap * cube(@w, @w, @h).center_xy.fillet(fill_r).translate(z: @h))
     end
-
-    #res += hull(cylinder(d1: @w, d2: @w-inset, h: 10, fn: 4), sphere(r: fill_r)).translate(x: - 30)
-
-    #res += sphere(r: fill_r).translate(x: - 30, z: gap_h/2.0)
-
     if @divet
       res -= sphere(d: @w/2.0)
     end
